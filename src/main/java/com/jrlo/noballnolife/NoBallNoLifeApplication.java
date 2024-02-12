93c14c812859f7fb88eb34a442d157161dde0133
LYP0416 <brandon416jr@gmail.com>
2024-02-12 17:50:11 +0800

    create my own project => no-ball-no-life

---
+++ src/main/java/com/jrlo/noballnolife/NoBallNoLifeApplication.java
@@ -0,0 +1,13 @@
+package com.jrlo.noballnolife;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoBallNoLifeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NoBallNoLifeApplication.class, args);
+    }
+
+}
